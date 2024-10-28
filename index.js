641ada049b0d9c29875f732dc47e580dbf502911
Pradeepkudipudi <pradeepk70220@gmail.com>
2024-10-28 11:57:03 +0530

    add

---
+++ index.js
@@ -0,0 +1,56 @@
+
+// 1
+age = "17"
+citizenship = "true"
+
+if (age>"18" && citizenship == "true" ){
+    console.log("can vote")
+} 
+ else  {
+    console.log("can not vote")
+}
+
+
+
+
+
+
+// 2
+
+age = "20"
+invitation = "true";
+
+if (age>= 21 || invitation == "true"){
+    console.log("canEnterClub")
+
+} else {
+    console.log("can not Enter club")
+}
+
+
+
+// 3
+
+member = false;
+age = 70 ;
+
+if (age>= 65 || member == true){
+
+    console.log("Eligible for discount")
+} else {
+    console.log("not eligible for discount")
+}
+
+
+//4
+
+gpa = 3.6;
+extracurriculars = true ;
+recommendation = false;
+
+if (gpa>=3.5 && extracurriculars == true || recommendation == true){
+    console.log("isEligible for scholarship")
+} else {
+    console.log("not eligible for scholarship")
+}
+
